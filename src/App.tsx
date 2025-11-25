cdfd8562a180e30d9e44072adcd652910c461851
Muhammad Akbar <m.akbar@neuralflex.io>
2025-11-25 14:53:18 +0300

    refactor: altered overall structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import HomePage from "./screens/HomePage";
+import InterviewForm from "./screens/InterviewForm";
+import MessageScreen from "./screens/MessageScreen";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import ReportView from "./screens/ReportView";
+import FeedbackScreen from "./screens/FeedbackScreen";
+import MessageScreenVoice from "./screens/MessageScreenVoice";
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/report" element={<InterviewForm />} />
+          <Route path="/messages-chat" element={<MessageScreen />} />
+          <Route path="/messages-voice" element={<MessageScreenVoice />} />
+
+          <Route path="/feedback" element={<FeedbackScreen />} />
+
+        </Routes>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
